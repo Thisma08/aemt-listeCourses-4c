55ce3ecde0dec3d5982a4a0fd64dd188e413c043
Mathis Mahaux <mathis.mahaux@gmail.com>
2024-11-14 18:04:32 +0100

    Adds the delete list feature

--- src/main/java/school/listecourses/exercicelistedecourses/application/listeCourses/commands/ListeCoursesCommandProcessor.java
+++ src/main/java/school/listecourses/exercicelistedecourses/application/listeCourses/commands/ListeCoursesCommandProcessor.java
@@ -3,20 +3,24 @@ package school.listecourses.exercicelistedecourses.application.listeCourses.comm
 import org.springframework.stereotype.Service;
 import school.listecourses.exercicelistedecourses.application.listeCourses.commands.create.ListeCoursesCreateCommand;
 import school.listecourses.exercicelistedecourses.application.listeCourses.commands.create.ListeCoursesCreateOutput;
+import school.listecourses.exercicelistedecourses.application.listeCourses.commands.delete.ListeCoursesDeleteCommand;
+import school.listecourses.exercicelistedecourses.application.listeCourses.commands.delete.ListeCoursesDeleteOutput;
 import school.listecourses.exercicelistedecourses.application.listeCourses.commands.update.ListeCoursesUpdateCommand;
 import school.listecourses.exercicelistedecourses.application.listeCourses.commands.update.ListeCoursesUpdateOutput;
 import school.listecourses.exercicelistedecourses.application.utils.ICommandHandler;
-import school.listecourses.exercicelistedecourses.application.utils.IEmptyOutputCommandHandler;
 
 @Service
 public class ListeCoursesCommandProcessor {
     private final ICommandHandler<ListeCoursesUpdateCommand, ListeCoursesUpdateOutput> updateListeCoursesHandler;
     private final ICommandHandler<ListeCoursesCreateCommand, ListeCoursesCreateOutput> createListeCoursesHandler;
+    private final ICommandHandler<ListeCoursesDeleteCommand, ListeCoursesDeleteOutput> deleteListeCoursesHandler;
 
 
-    public ListeCoursesCommandProcessor(ICommandHandler<ListeCoursesUpdateCommand, ListeCoursesUpdateOutput> updateListeCoursesHandler, ICommandHandler<ListeCoursesCreateCommand, ListeCoursesCreateOutput> createListeCoursesHandler) {
+
+    public ListeCoursesCommandProcessor(ICommandHandler<ListeCoursesUpdateCommand, ListeCoursesUpdateOutput> updateListeCoursesHandler, ICommandHandler<ListeCoursesCreateCommand, ListeCoursesCreateOutput> createListeCoursesHandler, ICommandHandler<ListeCoursesDeleteCommand, ListeCoursesDeleteOutput> deleteListeCoursesHandler) {
         this.updateListeCoursesHandler = updateListeCoursesHandler;
         this.createListeCoursesHandler = createListeCoursesHandler;
+        this.deleteListeCoursesHandler = deleteListeCoursesHandler;
     }
 
     public ListeCoursesUpdateOutput update(ListeCoursesUpdateCommand command) {
@@ -25,5 +29,9 @@ public class ListeCoursesCommandProcessor {
 
     public ListeCoursesCreateOutput create(ListeCoursesCreateCommand command) {
         return createListeCoursesHandler.handle(command);
+    }
+
+    public ListeCoursesDeleteOutput delete (ListeCoursesDeleteCommand command) {
+        return deleteListeCoursesHandler.handle(command);
     }
 }
---
+++ src/main/java/school/listecourses/exercicelistedecourses/application/utils/IEmptyOutputCommandHandler.java
@@ -0,0 +1,5 @@
+package school.listecourses.exercicelistedecourses.application.utils;
+
+public interface IEmptyOutputCommandHandler<I> {
+    void handle(I input);
+}
